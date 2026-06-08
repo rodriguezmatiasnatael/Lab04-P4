bf72aa432b007eeccbd37c4fa2b0ff833715b1aa
Matias Rodriguez <matiasnatael005@gmail.com>
2026-06-08 16:48:30 -0300

    Arreglo de errores del archivo fuente

--- src/DTDetalleViaje.cpp
+++ src/DTDetalleViaje.cpp
@@ -1,14 +1,15 @@
 #include "../include/DTDetalleViaje.h"
 
 DTDetalleViaje::DTDetalleViaje(int codigo, DTFecha fecha, std::string origen, std::string destino, int asientosPublicados, float precio, DTDetalleVehiculo vehi, std::vector<DTDetalleReserva> res)
-    {
-    this->codigo = codigo;
-    this->fecha = fecha;
-    this->origen = origen;
-    this->destino = destino;
-    this->asientosPublicados = asientosPublicados;
-    this->precio = precio;
-}, vehiculo(vehi), reservas(res) {}
+    : codigo(codigo), 
+      fecha(fecha), 
+      origen(origen), 
+      destino(destino), 
+      asientosPublicados(asientosPublicados), 
+      precio(precio), 
+      vehiculo(vehi), 
+      reservas(res) 
+{}
 
 int DTDetalleViaje::getCodigo() { return codigo; }
 DTFecha DTDetalleViaje::getFecha() { return fecha; }
