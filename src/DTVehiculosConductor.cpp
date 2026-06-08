0c402ee8fa5639c0e3d653e322a1142d3038df58
Matias Rodriguez <matiasnatael005@gmail.com>
2026-06-08 16:49:02 -0300

    Arreglo de errores del archivo fuente

--- src/DTVehiculosConductor.cpp
+++ src/DTVehiculosConductor.cpp
@@ -1,15 +1,15 @@
 #include "../include/DTVehiculosConductor.h"
 
-DTVehiculosConductor::DTVehiculosConductor(std::string matricula, std::string modelo, int capacidad){
+DTVehiculosConductor::DTVehiculosConductor(std::string matricula, std::string marca, int capacidad){
     this->matricula = matricula;
-    this->modelo = modelo;
+    this->marca = marca;
     this->capacidad = capacidad;
 }
 std::string DTVehiculosConductor::getMatricula(){
     return this->matricula;
 }
-std::string DTVehiculosConductor::getModelo(){
-    return this->modelo;
+std::string DTVehiculosConductor::getMarca(){
+    return this->marca;
 }
 int DTVehiculosConductor::getCapacidad(){
     return this->capacidad;
